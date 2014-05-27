27ca7be4373bb65e9352892c8ebf343d29e579a3
Ken Ward <kenward@Ken-Wards-MacBook.local>
2014-05-27 12:15:11 -0400

    added a test class

---
+++ Git Test/NTPTest.h
@@ -0,0 +1,13 @@
+//
+//  NTPTest.h
+//  Git Test
+//
+//  Created by Ken Ward on 5/27/14.
+//  Copyright (c) 2014 New Tech Pros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NTPTest : NSObject
+
+@end
---
+++ Git Test/NTPTest.m
@@ -0,0 +1,13 @@
+//
+//  NTPTest.m
+//  Git Test
+//
+//  Created by Ken Ward on 5/27/14.
+//  Copyright (c) 2014 New Tech Pros. All rights reserved.
+//
+
+#import "NTPTest.h"
+
+@implementation NTPTest
+
+@end
